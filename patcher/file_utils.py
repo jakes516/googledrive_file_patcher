409d8b3f2e7253a5cfd7b8415b11dfbeef1351e3
Jacob Read <jsread@carolina.rr.com>
2020-08-26 21:58:01 -0400

    Added function to automate file compression and create version.txt

--- patcher/file_utils.py
+++ patcher/file_utils.py
@@ -1,5 +1,6 @@
 import zipfile
 import os
+import re
 
 #unzips file and removes older files in directory
 def unzip_file(file, destination, deleteZip = True):
@@ -13,3 +14,28 @@ def unzip_file(file, destination, deleteZip = True):
 
     if deleteZip:
         os.remove(file)
+
+
+#TODO automate zipping and prompting version number
+def zip_file_with_VERSION(zip_name, file):
+    while True:
+
+            version_input = str(input("\nPlease type the version number in the form v#.#.#\n"))
+            regex = re.compile(r"^[vV](\d.){2}\d$")
+            if regex.match(version_input):
+                with open('VERSION_test.txt', 'w') as f:
+                    f.write(version_input)
+                print("Version number updated.")
+                break
+            else:
+                print("\nThe version number you entered is in an improper form. \nPlease re-enter the version number\n")
+                continue
+
+    with zipfile.ZipFile(zip_name, 'w') as myzip:
+
+        #enter arcname = to relative game file name
+        myzip.write(file, arcname='./wasp.jpg')
+        myzip.write('./VERSION_test.txt')
+        os.remove('./VERSION_test.txt')
+        #enter path to game projects directory
+#zip_file_with_VERSION('test_file.zip', "C:/Users/Jake/Pictures/wasp.jpg")
