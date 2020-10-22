17fbbbac08b5dbf6f9fd1e25add41dffd725d0a0
Jacob Read <jsread@carolina.rr.com>
2020-10-22 03:27:52 -0400

    auto version_history.yaml update

--- patcher/download_latest_file.py
+++
@@ -1,3 +0,0 @@
-from updater import download_latest_version
-
-download_latest_version()
--- patcher/test_yaml.py
+++
@@ -1,7 +0,0 @@
-import yaml
-yaml_version_format = {'version': [{'version.placeholder': {'file_id.placeholder': 'id.placeholder'}}],
-                       'version_history': [{'v.placeholder': {'file_id.placeholder': 'id.placeholder'}}]}
-yaml_string_format = str(yaml.dump(yaml_version_format))
-
-with open('versions_.yaml', 'w') as f:
-    f.write(yaml_string_format)
